9680a2872f2ca568cd5192cc9ad840803c4dafdd
Erin Downey <erindowney@Erins-MacBook-Pro.local>
2021-05-10 16:13:23 -0500

    deleted changes to js and reverted back to original code

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function compute()
 {
     var principal = document.getElementById("principal").value;
 
-    var rate = document.getElementById("rate").value;
+    var rate= document.getElementById("rate").value;
 
-    var years = document.getElementById("years").value;
+    var years= document.getElementById("years").value;
 
     var interest = principal * years * rate / 100;
 
