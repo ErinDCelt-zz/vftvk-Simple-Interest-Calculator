024085840679628d120d6c8a9e643c4ebbd30fea
Erin Downey <erindowney@Erins-MacBook-Pro.local>
2021-05-11 05:54:20 -0500

    updated per instructions

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
+function compute() {
+    p = document.getElementById("principal").value;
 
-    var rate= document.getElementById("rate").value;
+    r = document.getElementById("rate").value;
 
-    var years= document.getElementById("years").value;
+    y = document.getElementById("years").value;
+    result = document.getElementById("principal").value;
+
+    function num() {
+        var v = document.getElementById("principal").value;
+        if (v <= 0) {
+            alert("Value should be a positive number")
+        }
+    }
+
+    function sliderChange(val) {
+        document.getElementById("slider").value;
+    }
+
+    var dateobj = newDate();
+    var B = dateobj.getFullYear();
+    year = b + y;
 
     var interest = principal * years * rate / 100;
 
-    var year = new Date().getFullYear + parseInt(years);
-
-    function updateRate()
-    {
-        var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText = rateval;
-    }
-
-
+    var amt = interest;
+    var result = document.getElementById("result");
+    result.innerHTML = "If you deposit" + p + ",<br/> at an interest rate of" + r + "%,<br/>You will receive an amount of" + amt + ",<br/>in the year" + year;
 }
